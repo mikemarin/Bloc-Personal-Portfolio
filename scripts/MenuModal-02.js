0de3cc3aacc3a802fa85eb5193fc62653fdc6fd7
Mike Marin <mike.w.marin@gmail.com>
2020-04-09 22:46:38 -0500

    7.11 > Initial Submission

--- scripts/MenuModal-02.js
+++ scripts/MenuModal-02.js
@@ -1,8 +1,8 @@
 // Get the modal
-var modal = document.getElementById("myModal-01");
+var modal = document.getElementById("myModal-02");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn-01");
+var btn = document.getElementById("myBtn-02");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
